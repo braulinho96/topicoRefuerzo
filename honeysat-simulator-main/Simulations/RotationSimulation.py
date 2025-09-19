0e49f78ed84a24e1a6e5672914a5e8ec69923544
braulinho96 <braulio.bravo.c@usach.cl>
2025-09-19 11:25:00 -0300

    correccion del calculo de torque

--- honeysat-simulator-main/Simulations/RotationSimulation.py
+++ honeysat-simulator-main/Simulations/RotationSimulation.py
@@ -184,7 +184,7 @@ class RotationSimulation(Simulation):
 
     def d_omega(self, x_omega_b: np.array) -> np.array:
         h_total_b = self.moment_of_inertia.dot(x_omega_b)
-        w_dot = - self.inv_moment_of_inertia @ (np.cross(x_omega_b, h_total_b))
+        w_dot = - self.inv_moment_of_inertia @ (self.torque - np.cross(x_omega_b, h_total_b))
         return w_dot
 
     @staticmethod
